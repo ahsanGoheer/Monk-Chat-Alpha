9b63ab9d83cf951cd9a270dbcf9d94ad4530a63b
ahsanGoheer <ahsangoheer@live.com>
2021-06-21 05:09:13 +0500

    Added Profile Picture Functionality.

--- app/src/main/java/com/monk/monkchat/settings.java
+++ app/src/main/java/com/monk/monkchat/settings.java
@@ -1,5 +1,6 @@
 package com.monk.monkchat;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,22 +9,36 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.monk.monkchat.Models.Users;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class settings extends AppCompatActivity {
 
-    ImageView backArrowSettings,addImage,profilePicture;
+    ImageView backArrowSettings,addImage;
+    CircleImageView profilePicture;
     FirebaseStorage firebaseStorage;
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
+    EditText userNameSettings, AboutMeSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +50,35 @@ public class settings extends AppCompatActivity {
         firebaseDatabase=FirebaseDatabase.getInstance();
 
 
+
         getSupportActionBar().hide();
         backArrowSettings=(ImageView)findViewById(R.id.backArrowSettings);
         addImage=(ImageView)findViewById(R.id.addProfilePicture);
-        profilePicture=(ImageView)findViewById(R.id.profileImageSettings);
+        profilePicture=(CircleImageView)findViewById(R.id.profileImageSettings);
+
+
+        firebaseDatabase.getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                Users myUser= snapshot.getValue(Users.class);
+                Picasso.get().load(myUser.getProfilePic()).placeholder(R.drawable.user).into(profilePicture);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+
+
+
         backArrowSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
